0db3021d51499181db67e915abf2b6b9ca4c5750
Arturs Liepa <arturs.liepa85@gmail.com>
2026-06-29 18:10:38 +0300

    Fix the Technoligies controller path

--- src/Modules/Technologies/Routes/tech-routes.js
+++ src/Modules/Technologies/Routes/tech-routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { getTech } = require("../Controller/informationController");
+const { getTech } = require("../Controller/techController");
 
 router.get("/", getTech);
 
