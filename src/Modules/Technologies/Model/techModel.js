0eb9860825126b6173a720312c57a042a55130a5
Arturs Liepa <arturs.liepa85@gmail.com>
2026-07-14 01:35:25 +0300

    Fix navigation and technologies models - delete custom id key

--- src/Modules/Technologies/Model/techModel.js
+++ src/Modules/Technologies/Model/techModel.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const techSchema = new Schema({
-  id: { type: Number, required: true },
   name: { type: String, required: true },
   status: { type: String, required: true },
 });
